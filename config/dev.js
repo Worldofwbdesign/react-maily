c1067c8cb43581dc1de86be19115044b677c659b
Worldofwbdesign <worldofwbdesign@gmail.com>
2017-12-04 23:33:49 +0200

    added keys for stripe

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,7 @@ module.exports = {
   googleClientId: '722610882483-g9lpnp87v3v7nu325ai9rliua9o58d1r.apps.googleusercontent.com',
   googleClientSecret: '_sCRHfw9vDh5uNCEx7p_lw34',
   mongoURI: 'mongodb://emaily:password@ds149954.mlab.com:49954/maily-dev',
-  cookieKey: 'dhjgjkfgdyghrtfhjtyu6r4teljdgfsweq657345hffhjl'
+  cookieKey: 'dhjgjkfgdyghrtfhjtyu6r4teljdgfsweq657345hffhjl',
+  stripePublishableKey: 'pk_test_cFLRlVVLN8AF4y3QpSvqXDDe',
+  stripeSecretKey: 'sk_test_78oZBhagS5qHzbRC5t1RdTh7'
 }
