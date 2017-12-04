4847371760b6cd7c16d65a74d3c2d877e73043bf
Worldofwbdesign <worldofwbdesign@gmail.com>
2017-12-04 23:38:33 +0200

    remove config files

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientId: '722610882483-g9lpnp87v3v7nu325ai9rliua9o58d1r.apps.googleusercontent.com',
-  googleClientSecret: '_sCRHfw9vDh5uNCEx7p_lw34',
-  mongoURI: 'mongodb://emaily:password@ds149954.mlab.com:49954/maily-dev',
-  cookieKey: 'dhjgjkfgdyghrtfhjtyu6r4teljdgfsweq657345hffhjl',
-  stripePublishableKey: 'pk_test_cFLRlVVLN8AF4y3QpSvqXDDe',
-  stripeSecretKey: 'sk_test_78oZBhagS5qHzbRC5t1RdTh7'
-}
